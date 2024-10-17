b96f9ffe7176bd6d0e9e84c3947252f0d81163a8
vinayak soni <sonivinayak716@gmail.com>
2024-10-17 15:04:31 +0530

    uploaded

--- TicketbookingSystemJ531/src/com/hexaware/ticketbooking/tests/TicketBookingTests.java
+++ TicketbookingSystemJ531/src/com/hexaware/ticketbooking/tests/TicketBookingTests.java
@@ -1,6 +1,8 @@
 package com.hexaware.ticketbooking.tests;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class TicketBookingTests {
--- TicketbookingSystemJ531/src/module-info.java
+++ TicketbookingSystemJ531/src/module-info.java
@@ -2,4 +2,5 @@ module TicketbookingSystemJ531 {
 	
 	requires java.sql;
 	requires mysql.connector.j;
+	requires org.junit.jupiter.api;
 }
